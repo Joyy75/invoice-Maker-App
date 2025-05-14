4a9698288f2889c1ce5cfb6323248f4d1f9c0237
thiri thal su hlaing <thirithalsuhlaing@gmail.com>
2025-05-14 13:28:32 +0630

    v 1.0.1

--- src/states.js
+++ src/states.js
@@ -1,8 +1,9 @@
 export const products = [
   { id: 1, name: "Apple", price: 500 },
   { id: 2, name: "Banana", price: 200 },
-  { id: 3, name: "Orange", price: 700 },
-  
+  { id: 3, name: "Orange", price: 700 }, 
+  { id: 4, name: "Grapes", price: 800 },
+  { id: 5, name: "Mango", price: 600 },
 
  
  
